37eb1879925b36db54f883fb0b7002d8c0ed5f1c
Thiago Marques <thiagomarques408@gmail.com>
2022-10-12 18:28:32 -0300

    add search input

---
+++ api/app/controllers/name_statistics_controller.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class NameStatisticsController < ApplicationController
+
+end
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,31 @@
 import './App.css';
+import { useEffect, useState } from 'react';
+import Table from './Table';
+import axios from 'axios';
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios.get(`https://jsonmock.hackerrank.com/api/football_matches?team1=${query}`);
+      console.log(res.data.data)
+      console.log(query)
+      setData(res.data.data);
+    };
+    if (query.length === 0 || query.length > 1) fetchData();
+  }, [query]);
+
   return (
     <div className="App">
       <header className="App-header">
-       <input type="text" placeholder="Search" className="Search" />
+        <input
+          className="search"
+          placeholder="Search..."
+          onChange={(e) => setQuery(e.target.value.toLowerCase())}
+        />
+        {<Table data={data} />}
       </header>
     </div>
   );
